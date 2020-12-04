8cf118ebdc7482b0f6f7d691eeb47fbde17a191b
Derick Fay <derickfay@users.noreply.github.com>
2020-12-03 20:06:29 -0800

    Create fuzzy list-mf.py

---
+++ fuzzy list-mf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+import csv
+import sys
+import json
+import os
+
+theFile=sys.argv[1]
+fieldnames=["title","subtitle","arg"]
+
+json_filename = theFile.split(".")[0]+".json"
+
+
+def convert(csv_filename, json_filename, fieldnames):
+	f=open(csv_filename, 'r')
+	csv_reader = csv.DictReader(f,fieldnames)
+
+	jsonf = open(json_filename,'w') 
+	jsonf.write('{"items":[')
+
+	data=""
+
+	for r in csv_reader:
+		r['uid']=r['arg']
+		data = data+json.dumps(r)+",\n"
+	
+	jsonf.write(data[:-2]) 
+
+	jsonf.write(']}')
+	f.close()
+	jsonf.close()
+
+if (not os.path.isfile(json_filename)) or (os.path.getmtime(theFile) > os.path.getmtime(json_filename)) :
+	convert(theFile, json_filename, fieldnames)
+
+with open(json_filename, 'r') as fin:
+	print(fin.read(), end="")
